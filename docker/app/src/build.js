cd671a811f0df09d4b9174ce0375e0a14553d1e7
nokotan <kamenokonokotan@gmail.com>
2020-04-09 14:45:47 +0900

    Linker option change: FULL_ES3 to FULL_ES2

--- docker/app/src/build.js
+++ docker/app/src/build.js
@@ -97,7 +97,7 @@ function get_clang_options(options) {
 
 function get_lld_options(options) {
   // const clang_flags = `--target=wasm32-unknown-unknown-wasm --sysroot=${sysroot} -nostartfiles -Wl,--allow-undefined,--no-entry,--no-threads,--export-dynamic`;
-  const clang_flags = `-s MAIN_MODULE=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0`;
+  const clang_flags = `-s MAIN_MODULE=1 -s FULL_ES2=1 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0`;
   if (!options) {
     return clang_flags;
   }
